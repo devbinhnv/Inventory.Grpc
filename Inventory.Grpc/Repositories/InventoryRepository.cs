927f3ac994ba5a56898c18ed7b98210676e1c996
devbinhnv <dev.binhnv@gmail.com>
2025-07-12 22:43:40 +0700

    feat: define repository

---
+++ Inventory.Grpc/Repositories/InventoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using Infrastructure.Common;
+using Inventory.Grpc.Entities;
+using Inventory.Grpc.Repositories.Interfaces;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using Shared.Configurations;
+
+namespace Inventory.Grpc.Repositories;
+
+public class InventoryRepository : MongoDbRepository<InventoryEntry>, IInventoryRepository
+{
+    public InventoryRepository(IMongoClient client, IOptions<MongoDbSettings> option) : base(client, option)
+    {
+    }
+
+    public async Task<int> GetStockQuantity(string itemNo)
+        => Collection.AsQueryable()
+            .Where(x => x.ItemNo.Equals(itemNo))
+            .Sum(x => x.Quantity);
+}
