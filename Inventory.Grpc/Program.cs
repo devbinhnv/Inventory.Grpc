614bca4f0717dc0b8e4738d7c3ff526b225cf2d2
devbinhnv <dev.binhnv@gmail.com>
2025-07-12 22:44:30 +0700

    feat: define InventoryService extend from StockProto

--- Inventory.Grpc/Program.cs
+++ Inventory.Grpc/Program.cs
@@ -1,14 +1,42 @@
+using Common.Logging;
+using Inventory.Grpc.Extensions;
 using Inventory.Grpc.Services;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateBootstrapLogger();
+builder.Host.UseSerilog(SeriLogger.Configure);
 
+Log.Information($"Start {builder.Environment.ApplicationName} up");
+
+try
+{
 // Add services to the container.
-builder.Services.AddGrpc();
+    builder.Services.AddGrpc();
+    builder.Services.AddInfrastructureServices();
+    builder.Services.ConfigureMongoDb();
+    var app = builder.Build();
 
-var app = builder.Build();
+    // Configure the HTTP request pipeline.
+    app.MapGrpcService<InventoryService>();
+    app.MapGet("/",
+        () =>
+            "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
-// Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+    app.Run();
+}
+catch (Exception ex)
+{
+    string exceptionType = ex.GetType().Name;
+    if (exceptionType.Equals("StopTheHostException", StringComparison.Ordinal))
+    {
+        throw;
+    }
 
-app.Run();
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shutdown Ordering API complete");
+    Log.CloseAndFlush();
+}
---
+++ Inventory.Grpc/Services/InventoryService.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Core;
+using Inventory.Grpc.Protos;
+using Inventory.Grpc.Repositories.Interfaces;
+using ILogger = Serilog.ILogger;
+namespace Inventory.Grpc.Services;
+
+public class InventoryService : StockProtoService.StockProtoServiceBase
+{
+    private readonly IInventoryRepository _inventoryRepository;
+    private readonly ILogger _logger;
+
+    public InventoryService(IInventoryRepository inventoryRepository, ILogger logger)
+    {
+        _inventoryRepository = inventoryRepository ?? throw new ArgumentNullException(nameof(inventoryRepository));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    public override async Task<StockModel> GetStock(GetStockRequest request, ServerCallContext context)
+    {
+        _logger.Information($"BEGIN: Get stock of ItemNo {request.ItemNo}");
+        var stockQuantity = await _inventoryRepository.GetStockQuantity(request.ItemNo);
+        var result = new StockModel
+        {
+            Quantity = stockQuantity
+        };
+        _logger.Information($"END: Get stock of ItemNo {request.ItemNo} - Quantity:{result.Quantity}");
+
+        return result;
+    }
+}
