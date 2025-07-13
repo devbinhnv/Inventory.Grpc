3b426435338b8e32e06e4f624faa5ba0b0786a48
devbinhnv <dev.binhnv@gmail.com>
2025-07-13 19:02:06 +0700

    feat: add collection name

--- Inventory.Grpc/Entities/InventoryEntry.cs
+++ Inventory.Grpc/Entities/InventoryEntry.cs
@@ -1,9 +1,11 @@
 ﻿
 using Contracts.Domains;
+using Infrastructure.Extensions;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace Inventory.Grpc.Entities;
 
+[BsonCollection("InventoryEntries")]
 public class InventoryEntry : MongoEntity
 {
     [BsonElement("itemNo")] 
